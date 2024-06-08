12552a3de1a4e074baa8671889da734342260831
EinsteinGaming-beep <einsteingaming35@gmail.com>
2024-06-08 10:15:03 +0700

    Update beranda.py

--- pages/beranda.py
+++ pages/beranda.py
@@ -10,6 +10,7 @@ st.title("Selamat Datang di Kalkulator Indeks Massa Tubuh")
 st.write("Website ini membantu Anda menghitung Indeks Massa Tubuh (IMT) dan memahami maknanya. IMT adalah alat sederhana untuk menentukan apakah seseorang memiliki berat badan yang sehat, kekurangan berat badan, kelebihan berat badan, atau obesitas.")
 
 st.markdown("""Kategori IMT
+
 Berdasarkan nilai IMT, seseorang dapat dikategorikan ke dalam kelompok-kelompok berikut:
 - Kurus (Kekurangan Berat Badan): IMT < 18,5
 - Normal (Berat Badan Ideal): IMT 18,5 - 24,9
