d5f771fd593753de661934b56dd22b6419e02a9f
EinsteinGaming-beep <einsteingaming35@gmail.com>
2024-06-08 09:46:24 +0700

    Update info.py

--- pages/info.py
+++ pages/info.py
@@ -10,9 +10,9 @@ Indeks Massa Tubuh (IMT) atau Body Mass Index (BMI) adalah sebuah indikator yang
 2. Lalu mengapa kita harus mengecek Indeks Massa Tubuh?
 Memeriksa Indeks Massa Tubuh (IMT) penting karena beberapa alasan terkait kesehatan dan pencegahan penyakit. Berikut adalah beberapa alasan utama mengapa Anda harus memeriksa IMT Anda:
 
-- 1. Menilai Kesehatan Berat Badan
+1. Menilai Kesehatan Berat Badan
 
-- 2. Mengidentifikasi Risiko Penyakit
+2. Mengidentifikasi Risiko Penyakit
 IMT yang tidak berada dalam rentang normal dapat meningkatkan risiko berbagai penyakit, termasuk:
 - Penyakit Jantung dan Stroke: Kelebihan berat badan meningkatkan risiko kondisi kardiovaskular.
 - Diabetes Tipe 2: Obesitas merupakan faktor risiko utama untuk diabetes tipe 2.
@@ -20,18 +20,18 @@ IMT yang tidak berada dalam rentang normal dapat meningkatkan risiko berbagai pe
 - Beberapa Jenis Kanker: Obesitas dikaitkan dengan peningkatan risiko kanker tertentu, seperti kanker payudara dan usus besar.
 - Masalah Pernafasan: Berat badan berlebih dapat menyebabkan apnea tidur dan masalah pernapasan lainnya.
 
-- 3. Panduan untuk Perubahan Gaya Hidup
+3. Panduan untuk Perubahan Gaya Hidup
 Mengetahui IMT Anda dapat memberikan motivasi untuk melakukan perubahan gaya hidup yang diperlukan, seperti:
 - Diet Sehat: Mengadopsi pola makan yang seimbang untuk mencapai dan mempertahankan berat badan yang sehat.
 - Aktivitas Fisik: Meningkatkan aktivitas fisik untuk membantu mengelola berat badan.
 - Kebiasaan Sehat: Mengembangkan kebiasaan sehat lainnya yang mendukung penurunan berat badan atau pemeliharaan berat badan yang sehat.
 
-- 4. Pemantauan Kesehatan Jangka Panjang
+4. Pemantauan Kesehatan Jangka Panjang
 IMT adalah alat yang berguna untuk memantau perubahan berat badan dari waktu ke waktu. Ini penting untuk:
 Menilai Keberhasilan Program Penurunan Berat Badan: Melacak kemajuan dan keberhasilan program diet dan olahraga.
 Pemantauan Pertumbuhan: Pada anak-anak dan remaja, IMT digunakan untuk memastikan pertumbuhan dan perkembangan yang sehat.
 
-- 5. Membantu Diagnosis dan Pengobatan
+5. Membantu Diagnosis dan Pengobatan
 Profesional kesehatan menggunakan IMT sebagai bagian dari penilaian kesehatan secara keseluruhan. IMT dapat membantu dalam diagnosis dan pengelolaan kondisi kesehatan terkait berat badan. Ini memungkinkan dokter untuk memberikan rekomendasi yang lebih tepat dan personalisasi.
 
 Kesimpulan:
